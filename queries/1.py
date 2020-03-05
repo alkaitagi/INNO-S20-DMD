0493198e2ede5a88844814a630f4ad7a24742904
alkaitagi <alkaitagi@outlook.com>
2020-03-05 20:04:42 +0300

    CSV result improvements

--- queries/1.py
+++ queries/1.py
@@ -1,126 +1,125 @@
 from pymongo import MongoClient
 from datetime import datetime
+import csv
 
 client = MongoClient("localhost", 27017)
-database = client["dvdrental"]
-collection = database["customer"]
-
-pipeline = [
-    {
-        u"$project": {
-            u"_id": 0,
-            u"customer": u"$$ROOT"
-        }
-    },
-    {
-        u"$lookup": {
-            u"localField": u"customer.customer_id",
-            u"from": u"rental",
-            u"foreignField": u"customer_id",
-            u"as": u"rental"
-        }
-    },
-    {
-        u"$unwind": {
-            u"path": u"$rental",
-            u"preserveNullAndEmptyArrays": False
-        }
-    },
-    {
-        u"$lookup": {
-            u"localField": u"rental.inventory_id",
-            u"from": u"inventory",
-            u"foreignField": u"inventory_id",
-            u"as": u"inventory"
-        }
-    },
-    {
-        u"$unwind": {
-            u"path": u"$inventory",
-            u"preserveNullAndEmptyArrays": False
-        }
-    },
-    {
-        u"$lookup": {
-            u"localField": u"inventory.film_id",
-            u"from": u"film",
-            u"foreignField": u"film_id",
-            u"as": u"film"
-        }
-    },
-    {
-        u"$unwind": {
-            u"path": u"$film",
-            u"preserveNullAndEmptyArrays": False
-        }
-    },
-    {
-        u"$lookup": {
-            u"localField": u"film.film_id",
-            u"from": u"film_category",
-            u"foreignField": u"film_id",
-            u"as": u"film_category"
-        }
-    },
-    {
-        u"$unwind": {
-            u"path": u"$film_category",
-            u"preserveNullAndEmptyArrays": False
-        }
-    },
-    {
-        u"$match": {
-            u"rental.rental_date": {
-                u"$gte": datetime(2006, 1, 1)
-            }
-        }
-    },
-    {
-        u"$group": {
-            u"_id": {
-                u"customer\u1390customer_id": u"$customer.customer_id",
-                u"customer\u1390last_name": u"$customer.last_name",
-                u"customer\u1390first_name": u"$customer.first_name"
-            },
-            u"COUNT(film_category\u1390category_id)": {
-                u"$sum": 1
-            }
-        }
-    },
-    {
-        u"$project": {
-            u"customer.customer_id": u"$_id.customer\u1390customer_id",
-            u"customer.first_name": u"$_id.customer\u1390first_name",
-            u"customer.last_name": u"$_id.customer\u1390last_name",
-            u"COUNT(film_category.category_id)": u"$COUNT(film_category\u1390category_id)",
-            u"_id": 0
-        }
-    },
-    {
-        u"$match": {
-            u"COUNT(film_category.category_id)": {
-                u"$gte": 2
-            }
-        }
-    },
-    {
-        u"$project": {
-            u"_id": 0,
-            u"customer.customer_id": u"$customer.customer_id",
-            u"customer.first_name": u"$customer.first_name",
-            u"customer.last_name": u"$customer.last_name"
-        }
-    }
-]
+collection = client["dvdrental"]["customer"]
 
 cursor = collection.aggregate(
-    pipeline,
+    [
+        {
+            u"$project": {
+                u"_id": 0,
+                u"customer": u"$$ROOT"
+            }
+        },
+        {
+            u"$lookup": {
+                u"localField": u"customer.customer_id",
+                u"from": u"rental",
+                u"foreignField": u"customer_id",
+                u"as": u"rental"
+            }
+        },
+        {
+            u"$unwind": {
+                u"path": u"$rental",
+                u"preserveNullAndEmptyArrays": False
+            }
+        },
+        {
+            u"$lookup": {
+                u"localField": u"rental.inventory_id",
+                u"from": u"inventory",
+                u"foreignField": u"inventory_id",
+                u"as": u"inventory"
+            }
+        },
+        {
+            u"$unwind": {
+                u"path": u"$inventory",
+                u"preserveNullAndEmptyArrays": False
+            }
+        },
+        {
+            u"$lookup": {
+                u"localField": u"inventory.film_id",
+                u"from": u"film",
+                u"foreignField": u"film_id",
+                u"as": u"film"
+            }
+        },
+        {
+            u"$unwind": {
+                u"path": u"$film",
+                u"preserveNullAndEmptyArrays": False
+            }
+        },
+        {
+            u"$lookup": {
+                u"localField": u"film.film_id",
+                u"from": u"film_category",
+                u"foreignField": u"film_id",
+                u"as": u"film_category"
+            }
+        },
+        {
+            u"$unwind": {
+                u"path": u"$film_category",
+                u"preserveNullAndEmptyArrays": False
+            }
+        },
+        {
+            u"$match": {
+                u"rental.rental_date": {
+                    u"$gte": datetime(2006, 1, 1)
+                }
+            }
+        },
+        {
+            u"$group": {
+                u"_id": {
+                    u"customer\u1390customer_id": u"$customer.customer_id",
+                    u"customer\u1390last_name": u"$customer.last_name",
+                    u"customer\u1390first_name": u"$customer.first_name"
+                },
+                u"COUNT(film_category\u1390category_id)": {
+                    u"$sum": 1
+                }
+            }
+        },
+        {
+            u"$project": {
+                u"customer.customer_id": u"$_id.customer\u1390customer_id",
+                u"customer.first_name": u"$_id.customer\u1390first_name",
+                u"customer.last_name": u"$_id.customer\u1390last_name",
+                u"COUNT(film_category.category_id)": u"$COUNT(film_category\u1390category_id)",
+                u"_id": 0
+            }
+        },
+        {
+            u"$match": {
+                u"COUNT(film_category.category_id)": {
+                    u"$gte": 2
+                }
+            }
+        },
+        {
+            u"$project": {
+                u"_id": 0,
+                u"first_name": u"$customer.first_name",
+                u"last_name": u"$customer.last_name"
+            }
+        }
+    ],
     allowDiskUse=True
 )
-print("Rented more than 2 categories of movies:")
-try:
-    with open('results/1.txt', 'w') as file:
-        for doc in cursor:
-            file.write(doc["customer"]["first_name"] + " " + doc["customer"]["last_name"] + "\n")
-finally:
-    client.close()
+
+records = []
+for doc in cursor:
+    records.append(doc)
+client.close()
+
+with open("results/1.csv", "w") as file:
+    csv.writer(file, lineterminator='\n').writerows(
+        [f'{r["first_name"]} {r["last_name"]}'] for r in records)
--- queries/2.py
+++ queries/2.py
@@ -1,65 +1,90 @@
 from pymongo import MongoClient
-from prettytable import PrettyTable
+import csv
 
 client = MongoClient("localhost", 27017)
-database = client["dvdrental"]
-collection = database["film_actor"]
-
-pipeline = [
-    {
-        u"$project": {
-            u"_id": 0,
-            u"F1": u"$$ROOT"
-        }
-    },
-    {
-        u"$lookup": {
-            u"localField": u"F1.film_id",
-            u"from": u"film_actor",
-            u"foreignField": u"film_id",
-            u"as": u"F2"
-        }
-    },
-    {
-        u"$unwind": {
-            u"path": u"$F2",
-            u"preserveNullAndEmptyArrays": False
-        }
-    },
-    {
-        u"$group": {
-            u"_id": {
-                u"actor1": u"$F1.actor_id",
-                u"actor2": u"$F2.actor_id"
-            },
-            u"films": {
-                u"$sum": 1
+collection = client["dvdrental"]["film_actor"]
+cursor = collection.aggregate(
+    [
+        {
+            u"$project": {
+                u"_id": 0,
+                u"F1": u"$$ROOT"
+            }
+        },
+        {
+            u"$lookup": {
+                u"localField": u"F1.film_id",
+                u"from": u"film_actor",
+                u"foreignField": u"film_id",
+                u"as": u"F2"
+            }
+        },
+        {
+            u"$unwind": {
+                u"path": u"$F2",
+                u"preserveNullAndEmptyArrays": False
+            }
+        },
+        {
+            u"$group": {
+                u"_id": {
+                    u"actor1": u"$F1.actor_id",
+                    u"actor2": u"$F2.actor_id"
+                },
+                u"films": {
+                    u"$sum": 1
+                }
+            }
+        },
+        {
+            u"$project": {
+                u"_id": 0,
+                u"actor1": u"$_id.actor1",
+                u"actor2": u"$_id.actor2",
+                u"films": u"$films"
+            }
+        },
+        {
+            u"$sort": {
+                "actor1": 1,
+                "actor2": 1
             }
         }
-    },
-    {
-        u"$project": {
-            u"_id": 0,
-            u"actor1": u"$_id.actor1",
-            u"actor2": u"$_id.actor2",
-            u"films": u"$films"
-        }
-    },
-    {
-        u"$sort": {
-            "actor1": 1,
-            "actor2": 1
-        }
-    }
-]
-
-cursor = collection.aggregate(
-    pipeline,
+    ],
     allowDiskUse=True
 )
 
-try:
-    for doc in cursor:
-        print(doc)
-finally:
-    client.close()
+
+records = []
+for doc in cursor:
+    records.append(doc)
+client.close()
+
+
+with open("results/2.csv", "w") as file:
+    sheet = csv.writer(file, lineterminator='\n')
+
+    max_id = max(r["actor1"] for r in records)
+    id1, id2 = 0, max_id + 1
+    row = ["ID"] + list(range(1, max_id + 1))
+    for r in records:
+        if r["actor1"] != id1:
+            if id2 <= max_id:
+                row += [0] * (max_id - id2 + 1)
+            sheet.writerow(row)
+
+            id1 += 1
+            id2 = 1
+            row = [id1]
+
+        if r["actor2"] != id2:
+            row += [0] * (r["actor2"] - id2)
+            id2 = r["actor2"]
+
+        row.append(r["films"])
+        id2 += 1
+
+    if len(row) > 1:
+        if id2 <= max_id:
+            row += [0] * (max_id - id2 + 1)
+        sheet.writerow(row)
