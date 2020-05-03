0f84751b028468838ad3c7f1d900f574db5cd82d
alkaitagi <alkaitagi@outlook.com>
2020-05-03 23:34:46 +0300

    Recalculate 2nd & 3rd queries

--- 2/MAGOMED_MAGOMEDOV.sql
+++ 2/MAGOMED_MAGOMEDOV.sql
@@ -2,6 +2,13 @@ CREATE INDEX query_0_52 ON rental (rental_id, customer_id, staff_id, last_update
 CREATE INDEX query_0_66 ON rental (last_update, customer_id, staff_id, rental_id);
 CREATE INDEX query_0_72 ON payment (rental_id, payment_date);
 
-CREATE INDEX query_1_5 ON film (release_year, rental_rate);
+CREATE INDEX query_1_11 ON film (rental_rate, release_year);
 
-CREATE INDEX query_2_26 ON inventory (film_id);
+CREATE INDEX query_2_97 ON inventory (film_id, inventory_id);
+CREATE INDEX query_2_89 ON rental (inventory_id, customer_id, rental_id);
+CREATE INDEX query_2_99 ON payment USING hash (rental_id);
+CREATE INDEX query_2_104 ON film_actor (film_id, actor_id);
+CREATE INDEX query_2_71 ON customer USING hash (first_name);
+CREATE INDEX query_2_85 ON rental (customer_id, inventory_id);
+CREATE INDEX query_2_93 ON inventory USING hash (inventory_id);
+CREATE INDEX query_2_61 ON film (film_id, rating, release_year, title);
